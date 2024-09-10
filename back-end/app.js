b56d7e5e001fc5652325da49698691f502a0d140
Tissotkevin <tissot.phi.kevin@laposte.net>
2024-09-10 14:26:07 +0200

    suite

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://tissotkevin:DDazzle971@cluster0.qn4eg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,243 +20,9 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-        {
-        "id": "1",
-        "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-        "title" : "Milwaukee Mission",
-        "author": "Elder Cooper",
-        "imageUrl" : "https://via.placeholder.com/206x260",
-        "year" : 2021,
-        "genre" : "Policier",
-        "ratings" : [{
-          "userId" : "1",
-          "grade": 5
-        },
-          {
-            "userId" : "1",
-            "grade": 5
-          },
-          {
-            "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-            "grade": 5
-          },
-          {
-            "userId" : "1",
-            "grade": 5
-          }],
-        "averageRating": 3
-      },
-        {
-          "id": "2",
-          "userId" : "clbxs3tag6jkr0biul4trzbrv",
-          "title" : "Book for Esther",
-          "author": "Alabaster",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Paysage",
-          "ratings" : [{
-            "userId" : "clbxs3tag6jkr0biul4trzbrv",
-            "grade": 4
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 4.2
-        },
-        {
-          "id": "3",
-          "userId" : "1",
-          "title" : "The Kinfolk Table",
-          "author": "Nathan Williams",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Cuisine",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "4",
-          "userId" : "1",
-          "title" : "Milwaukee Mission",
-          "author": "Elder Cooper",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2021,
-          "genre" : "Policier",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "5",
-          "userId" : "1",
-          "title" : "Book for Esther",
-          "author": "Alabaster",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Paysage",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 4
-        },
-        {
-          "id": "6",
-          "userId" : "1",
-          "title" : "The Kinfolk Table",
-          "author": "Nathan Williams",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Cuisine",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "7",
-          "userId" : "1",
-          "title" : "Milwaukee Mission",
-          "author": "Elder Cooper",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2021,
-          "genre" : "Policier",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 3
-        },
-        {
-          "id": "8",
-          "userId" : "clc7s9xnh7zpt0ak4fisdwuj1",
-          "title" : "Book for Esther",
-          "author": "Alabaster",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Paysage",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            }],
-          "averageRating": 4
-        },
-        {
-          "id": "9",
-          "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-          "title" : "The Kinfolk Table",
-          "author": "Nathan Williams",
-          "imageUrl" : "https://via.placeholder.com/206x260",
-          "year" : 2022,
-          "genre" : "Cuisine",
-          "ratings" : [{
-            "userId" : "1",
-            "grade": 5
-          },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "1",
-              "grade": 5
-            },
-            {
-              "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-              "grade": 1
-            }],
-          "averageRating": 3
-        }
-      ]
-    res.status(200).json(stuff);
-  });
+  app.use(bodyParser.json());
+
+  app.use('/api/stuff', stuffRoutes);
+
+
 module.exports = app;
